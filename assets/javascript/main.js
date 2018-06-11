f9df758d0a46aaeb1b41e523cc52ea274cebe69f
arjunalapsapkota <arjunalapsapkota@gmail.com>
2018-06-10 23:28:28 -0700

    choose-character

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,10 +1,39 @@
-$(document).ready(function(){
+ $(document).ready(function(){
 // Jquerry code goes in here
-var flag=0;
-$("#playNow, #reset").on("click",function(){
-$("#description").hide();
-$("#characterList").hide();
 
+
+$("playNow").hide();
+
+$(".image-holder").on("click",function(){
+   var store=this.id;
+   $("#chooseCharacter").empty();
+   $("#chooseCharacter").append("Your Character");
+   
+    if(store==="arjun")
+    {
+      $("#bhim").hide();
+      $("#duryo").hide();
+      $("#karna").hide();
+      
+    }
+    if(store==="bhim")
+    {
+        $("#arjun").hide();
+        $("#duryo").hide();
+        $("#karna").hide();
+    }
+    if(store==="duryo")
+    {
+        $("#arjun").hide();
+        $("#bhim").hide();
+        $("#karna").hide();
+    }
+    if(store==="karna")
+    {
+        $("#arjun").hide();
+        $("#bhim").hide();
+        $("#duryo").hide();
+    }
 });
 
 
