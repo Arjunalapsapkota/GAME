bac754f98a238dabe1ed02b35f78b0a781bab4c8
arjunalapsapkota <arjunalapsapkota@gmail.com>
2018-06-10 19:32:10 -0700

    game-intro

---
+++ assets/javascript/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+// Jquerry code goes in here
+var flag=0;
+$("#playNow, #reset").on("click",function(){
+$("#description").hide();
+$("#characterList").hide();
+
+});
+
+
+
+
+
+
+});
