44ac1382674eeefe32497d4354b61b969d2c0991
arjunalapsapkota <arjunalapsapkota@gmail.com>
2018-06-13 09:45:49 -0700

    button-included

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,39 +1,88 @@
  $(document).ready(function(){
 // Jquerry code goes in here
-
+var flag=0;
+var player="";
+var counter=3;
+var audioElement = document.createElement("audio");
+audioElement.setAttribute("src", "assets/sword.wav");
+var button=$("<button>");
+button.addClass("btn btn-primary");
 
 $("playNow").hide();
 
 $(".image-holder").on("click",function(){
-   var store=this.id;
-   $("#chooseCharacter").empty();
-   $("#chooseCharacter").append("Your Character");
+   //Character Selection
    
-    if(store==="arjun")
+   player=this.id;
+   if(flag==1)
+   {
+    button.empty();
+    button.append("attack!");
+    button.attr("id","fight");
+    $("#attackbutton").append(button);
+    $("#fight").on("click",function(){
+        audioElement.play();
+       
+    })
+    counter--;
+    if(counter==0) $("#choseOpponents").hide();
+    $("#enemy").empty();
+    $("#enemy").append("opponent");
+    $("#opponent").empty(); 
+    if(player==="arjun")
     {
-      $("#bhim").hide();
-      $("#duryo").hide();
-      $("#karna").hide();
+    
+    $("#opponent").append($("#arjun"));
+   
+    }
+    if(player==="bhim")
+    {
+    $("#opponent").append($("#bhim"));
+    
+    }
+    if(player==="duryo")
+    {
+    $("#opponent").append($("#duryo"));  
+   
+    }
+    if(player==="karna")
+    {
+    $("#opponent").append($("#karna"));
+    }
+   }
+   if(flag==0)
+   {
+    flag=1;
+    $("#chooseCharacter").empty();
+    $("#chooseCharacter").append("Your Character");
+    $("#enemies").append("Choose your Opponent")
+    if(player==="arjun")
+    {
+    $("#opponents").append($("#duryo"));
+    $("#opponents").append($("#karna"));
+    $("#opponents").append($("#bhim"));
       
     }
-    if(store==="bhim")
+    if(player==="bhim")
     {
-        $("#arjun").hide();
-        $("#duryo").hide();
-        $("#karna").hide();
+    $("#opponents").append($("#duryo"));
+    $("#opponents").append($("#karna"));
+    $("#opponents").append($("#arjun"));
     }
-    if(store==="duryo")
+    if(player==="duryo")
     {
-        $("#arjun").hide();
-        $("#bhim").hide();
-        $("#karna").hide();
+    $("#opponents").append($("#arjun"));  
+    $("#opponents").append($("#karna"));
+    $("#opponents").append($("#bhim"));
     }
-    if(store==="karna")
+    if(player==="karna")
     {
-        $("#arjun").hide();
-        $("#bhim").hide();
-        $("#duryo").hide();
+    $("#opponents").append($("#duryo"));
+    $("#opponents").append($("#arjun"));
+    $("#opponents").append($("#bhim"));
     }
+   }
+   
 });
 
 
