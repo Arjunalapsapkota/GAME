014541cc7fe303fc078d2975e1a593a7c82d3844
arjunalapsapkota <arjunalapsapkota@gmail.com>
2018-06-14 20:12:56 -0700

    typo

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -2,56 +2,82 @@
 // Jquerry code goes in here
 var flag=0;
 var player="";
+var opponent="";
 var counter=3;
+var count=0;
+var power=120;
+var playerPower=120
+var opponentPower=120;                                  // 120 HP for all characters
 var audioElement = document.createElement("audio");
 audioElement.setAttribute("src", "assets/sword.wav");
 var button=$("<button>");
 button.addClass("btn btn-primary");
-
 $("playNow").hide();
+
+/* Attack function goes below */
+/* Function defination goes here */
+
+
+function attackFunction(){
+    var randomNumber= function(){return Math.floor(Math.random()*4 +1);}
+    console.log(randomNumber());
+    var call=0;
+    
+    
+    var number=randomNumber();
+    var playerSword= function()
+    {
+        while (call<=number)
+        {
+            call=randomNumber();
+        }
+        return call*5;
+         
+    }
+    var opponentSword= function(){}
+    playerPower=playerPower-opponentSword();
+    console.log(playerPower);
+    opponentSword=opponentPower-playerSword();
+    
+}
+
+
+/* ####################################   */
+
+
 
 $(".image-holder").on("click",function(){
    //Character Selection
    
-   player=this.id;
+   
    if(flag==1)
    {
+    opponent=this.id;
     button.empty();
     button.append("attack!");
     button.attr("id","fight");
     $("#attackbutton").append(button);
-    $("#fight").on("click",function(){
+    $("#fight").on("click",function()           //starts the attack on the press of a button
+    {
         audioElement.play();
-       
+        attackFunction();
+        
     })
     counter--;
-    if(counter==0) $("#choseOpponents").hide();
+    
+    if(counter==0)                              //selects the opponent
+    {
+        $("#choseOpponents").hide();
+    }
     $("#enemy").empty();
     $("#enemy").append("opponent");
     $("#opponent").empty(); 
-    if(player==="arjun")
-    {
-    
-    $("#opponent").append($("#arjun"));
-   
-    }
-    if(player==="bhim")
-    {
-    $("#opponent").append($("#bhim"));
-    
-    }
-    if(player==="duryo")
-    {
-    $("#opponent").append($("#duryo"));  
-   
-    }
-    if(player==="karna")
-    {
-    $("#opponent").append($("#karna"));
-    }
+    var string="#"+opponent;
+    $("#opponent").append($(string));
    }
-   if(flag==0)
+   if(flag==0)                                 //Selects the character
    {
+    player=this.id;
     flag=1;
     $("#chooseCharacter").empty();
     $("#chooseCharacter").append("Your Character");
@@ -87,7 +113,28 @@ $(".image-holder").on("click",function(){
 
 
 
-
-
-
 });
+
+
+
+// if(true)    code that selects the opponent
+//         {
+//         if(opponent==="arjun")
+//         { 
+//         $("#opponent").append($("#arjun"));
+//         }
+//         if(opponent==="bhim")
+//         {
+//         $("#opponent").append($("#bhim"));
+        
+//         }
+//         if(opponent==="duryo")
+//         {
+//         $("#opponent").append($("#duryo"));  
+       
+//         }
+//         if(opponent==="karna")
+//         {
+//         $("#opponent").append($("#karna"));
+//         }
+//         }
